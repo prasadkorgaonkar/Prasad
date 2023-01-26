b6dcc38067549b0dc0b0af029d2019fc04455766
prasadkorgaonkar <119840587+prasadkorgaonkar@users.noreply.github.com>
2023-01-26 15:50:18 +0530

    Commited by Prasad

--- SecondLargestWithoutForLoop.java
+++ SecondLargestWithoutForLoop.java
@@ -8,10 +8,6 @@ public class SecondLargestWithoutForLoop {
 		int arr []= {25,10,5,80,30,40};
 		int n = arr.length;
 		Arrays.sort(arr);
-//		for(int i=0; i<arr.length; i++) {
-//		System.out.println(arr[i]+" ");
-//		
-//		}
-		System.out.print("Second Largest Number is: "+arr[n-2]);
+		System.out.print("Second Largest Number: "+arr[n-2]);
 	}
 }
